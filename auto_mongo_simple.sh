73c637a38e04de8707bb592b96a3111cc7ff44ab
mcravtov <mcravtov@gmail.com>
2018-01-16 16:24:21 +0200

    Add coments

--- auto_mongo_simple.sh
+++ auto_mongo_simple.sh
@@ -4,11 +4,15 @@ MONGO_DATABASE="your_db_name"
 #name of app stored in DB
 APP_NAME="your_app_name"
 
+# DB host
 MONGO_HOST="127.0.0.1"
 MONGO_PORT="27017"
+#Date,Time format
 TIMESTAMP=`date +%F-%H%M`
+#Path to mongodump util.
 MONGODUMP_PATH="/usr/bin/mongodump"
-BACKUPS_DIR="/home/username/backups/$APP_NAME"
+#Backup path
+BACKUPS_DIR="/backups/$APP_NAME"
 BACKUP_NAME="$APP_NAME-$TIMESTAMP"
  
 # mongo admin --eval "printjson(db.fsyncLock())"
